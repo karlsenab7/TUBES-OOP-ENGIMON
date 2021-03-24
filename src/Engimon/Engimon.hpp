53729e1d091bff606c72759e165973ef83cb1832
SleipnirMk1 <ilyasalefi@gmail.com>
2021-03-24 21:18:07 +0700

    Update Engimon, biar bisa compile dulu

--- src/Engimon/Engimon.hpp
+++ src/Engimon/Engimon.hpp
@@ -3,69 +3,76 @@
 
 #include <vector>
 #include <iostream>
-#include "../Skill.hpp"
+//#include "../Skill.hpp" //Menunggu skill
 #include "EngimonBase.hpp"
+//#include "EngimonBase.cpp"
 
 using namespace std;
 
 class Engimon : public EngimonBase {
     private:
         string species_name;
-        vector<Skill> engimon_skills;
+        //vector<Skill> engimon_skills;
 
     public:
         //Constructor
         Engimon() : EngimonBase() {
             species_name = "-";
-            engimon_skills.push_back("Tackle"); //ubah argumen string menjadi skill
+            //engimon_skills.push_back("Tackle"); //ubah argumen string menjadi skill
         }
 
-        Engimon(string name, string * parents, vector<string> elements, int _level, int _exp, int _cum_exp, string species, vector<Skill> skills) : EngimonBase(name, parents, elements, _level, _exp, _cum_exp) {
+        Engimon(string name, string * parents, vector<string> elements, int _level, int _exp, int _cum_exp, string species) : EngimonBase(name, parents, elements, _level, _exp, _cum_exp) {
             species_name = species;
-            engimon_skills = skills;
         }
+
+        //Engimon(string name, string * parents, vector<string> elements, int _level, int _exp, int _cum_exp, string species, vector<Skill> skills) : EngimonBase(name, parents, elements, _level, _exp, _cum_exp) {
+            //species_name = species;
+            //engimon_skills = skills;
+        //}
 
         Engimon(const Engimon& other) : EngimonBase(other) {
             species_name = other.species_name;
-            engimon_skills = other.engimon_skills;
+            //engimon_skills = other.engimon_skills;
         }
 
         //Destructor
-        ~Engimon() : ~EngimonBase() {
-            ~engimon_skills;
-        }
+        //~Engimon() : {
+            //delete[] engimon_parents;
+            //~engimon_elements;
+            //~engimon_skills;
+        //}
 
         //Getter
         string get_species_name() { return species_name; }
-        vector<Skill> get_engimon_skills() { return engimon_skills; }
+        //vector<Skill> get_engimon_skills() { return engimon_skills; }
 
         //Setter
         void set_species_name(string species) { species_name = species; }
-        void set_engimon_skills(vector<Skill> skills) { engimon_skills = skills; }
+        //void set_engimon_skills(vector<Skill> skills) { engimon_skills = skills; }
 
         //Additional methods
-        void add_skill(Skill skill) {
-            if (engimon_skills.size < 4)
-            {
-                engimon_skills.push_back(skill);
-            } else
-            {
-                throw "skill full"; //Mungkin penanganan catch-nya diarahkan ke prosedur change_skill
-            }
-        }
+        //void add_skill(Skill skill) {
+            //if (engimon_skills.size < 4)
+            //{
+                //engimon_skills.push_back(skill);
+            //} else
+            //{
+                //throw "skill full"; //Mungkin penanganan catch-nya diarahkan ke prosedur change_skill
+            //}
+        //}
 
         //Prekondisi skill engimon sudah 4 (penuh)
-        void change_skill(Skill skill, Skill * replaced) {
-            for (int i = 0; i < 4; i++) {
-                if (engimon_skills.at(i) == *replaced)
-                {
-                    engimon_skills.at(i) = skill;
-                    return;
-                }
-            }
+        //void change_skill(Skill skill, Skill * replaced) {
+            //for (int i = 0; i < 4; i++) {
+                //if (engimon_skills.at(i) == *replaced)
+                //{
+                    //engimon_skills.at(i) = skill;
+                    //return;
+                //}
+            //}
 
-            throw "No such skill";
-        }
+            //throw "No such skill";
+        //}
 
 };
 
