ffeabb96ec776f25152c1aa9463b53ded16bb4fb
MDrFaza <mdrfaza21@gmail.com>
2021-03-25 02:11:51 +0700

    Skill (sementara)

---
+++ src/Skill.hpp
@@ -0,0 +1,38 @@
+#ifndef Skill_HPP
+#define Skill_HPP
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Skill {
+    private:
+        string ID;
+        string name;
+        string element;
+        int* learnableBy; 
+        int power;
+        int mastery;
+        string description;
+        bool isUnique;
+        string uniqueTo;
+    public:
+        //Konstruktor
+        Skill(string ID, string name, string element, int* learnableBy, int power, string desc, bool isUnique, string uniqueTo);
+        //CC
+        Skill(const Skill& s);
+        //OpAssign
+        Skill& operator=(const Skill& s);
+        //Destruktor
+        ~Skill();
+        // getter
+        string getID();
+        string getName();
+        string getElement();
+        int getPower();
+        int getMastery();
+        string getDescription();
+        // Lain-Lain
+        void masteryUp();
+        bool canBeLearned(string element);
+};
+#endif
